0687aa19ed497746d0f709462b4e1966cd7e0fcc
Berkant Akyuz <berkant.akyuz@caretta.net>
2025-02-03 11:56:46 +0300

    Enums eklendi.

---
+++ PhoneBook.Common/Enums/ContactInfoTypeEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.Common.Enums
+{
+    public enum ContactInfoTypeEnum
+    {
+        Location = 1,
+        PhoneNumber = 2,
+        MailAddress = 3
+    }
+}
---
+++ PhoneBook.Common/Enums/ReportStatusEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.Common.Enums
+{
+    public enum ReportStatusEnum
+    {
+        Pending = 1,
+        Completed = 2,
+        Failed = 3
+    }
+}
